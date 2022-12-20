a061791be3121c74bd74d42b705356cd18738f82
chipmunkcol <jack95711@gmail.com>
2022-12-20 23:57:34 +0900

    selector

--- src/pages/TodoList.tsx
+++ src/pages/TodoList.tsx
@@ -1,24 +1,43 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useRecoilState } from "recoil";
 import styled from "styled-components";
+import { atom } from "recoil"
+
+
 
 function TodoList(){
 
-const [text, setText] = useState('')
-const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log(text)
+interface Itodo {
+    text: string;
+    category: "DONE" | "TO_DO"
 }
+const todoState = atom<Itodo[]>({
+    key: 'todoState',
+    default: []
+})
+
+
+const [todoList, setTodoList] = useRecoilState(todoState)
+console.log('todoList: ', todoList);
+
+const { register, handleSubmit, setValue } = useForm<{todo : string}>()
+
+const onSubmit = (data: {todo: string}) => {
+    setTodoList((prev) => [...prev, {
+        text: data.todo,
+        category: "TO_DO"
+    }])
+    setValue('todo',"")
+}
+
     return(
         <Wrap>
-            <form onSubmit={onSubmit}>
-                <input 
-                placeholder="write hello, world"
-                onChange={e => setText(e.target.value)}
-                value={text}
-                /> 
-                <button type="submit">Submit 버튼</button>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input {...register("todo")}/>
+                <button>submit</button>
             </form>
-            
+            {todoList.map( todo => <div>{todo.text}</div> )}
         </Wrap>
     )
 }
@@ -29,6 +48,7 @@ const Wrap = styled.div`
 width: 100vw;
 height: 100vh;
 display: flex;
+flex-direction: column;
 align-items: center;
 justify-content: center;
 `
